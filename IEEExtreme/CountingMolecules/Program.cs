42b4cd6cbc10f1d6f09ead35033d462e85c5dce8
Israel Zablianov <123hartk123@gmail.com>
2017-03-11 17:17:54 +0200

    Counting Molecules

---
+++ IEEExtreme/CountingMolecules/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingMolecules
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] values = input.Split(' ');
+            Console.WriteLine(Test(double.Parse(values[0]), double.Parse(values[1]), double.Parse(values[2])));
+        }
+
+        public static string Test(double C, double H, double O)
+        {
+            double water = 0, carbon = 0, glucose = 0;
+
+            carbon = ((2 * O) - H) / 4;
+            glucose = (C - (((2 * O) - H) / 4)) / 6;
+            water = O - C - ((2 * O) - H) / 4;
+
+            if(water == (long)water && glucose == (long)glucose && carbon == (long)carbon)
+            {
+                return $"{water} {carbon} {glucose}";
+            }
+
+            return "Error";
+        }
+    }
+}

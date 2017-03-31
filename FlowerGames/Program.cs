14fdb14d4e24739f2e065adf3f4e4a9ac91f9d0f
Israel Zablianov <123hartk123@gmail.com>
2017-04-01 00:48:12 +0300

    Flower Games

---
+++ FlowerGames/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerGames
+{
+    class Program
+    {
+        private static ulong powOf63 = 9223372036854775808;
+
+        static void Main(string[] args)
+        {
+            var tests = ulong.Parse(Console.ReadLine());
+            var answers = new List<string>(100);
+
+            for (ulong i = 0; i < tests; i++)
+            {
+                answers.Add(SingleTest().ToString());
+            }
+
+            foreach (var answer in answers)
+            {
+                Console.WriteLine(answer);
+            }
+        }
+
+        public static ulong FindClosestPowOfTwo(ulong amountOfPetals)
+        {
+            ulong closestPowOfTwo = 1;
+
+            while (closestPowOfTwo <= amountOfPetals)
+            {
+                closestPowOfTwo *= 2;
+            }
+
+            return closestPowOfTwo / 2;
+        }
+
+        public static ulong SingleTest()
+        {
+            ulong amountOfPetals = ulong.Parse(Console.ReadLine());
+
+            if (powOf63 == amountOfPetals)
+            {
+                return 1;
+            }
+
+            ulong startCountFrom = FindClosestPowOfTwo(amountOfPetals);
+            ulong endPosition = 1 + (amountOfPetals - startCountFrom) * 2;
+
+            return endPosition;
+        }
+    }
+}

7c2a677ed3aec4bde2e1bb69d366b8295982894a
Israel Zablianov <123hartk123@gmail.com>
2017-03-31 23:51:05 +0300

    stil not perfect

--- FoodTruck/Program.cs
+++ FoodTruck/Program.cs
@@ -21,55 +21,56 @@ namespace FoodTruck
         public static double r = 6378.137;
         public static IFormatProvider culture = new CultureInfo("en-US", true);
         public static string format = "MM/dd/yyyy HH:mm";
-        public static double lat1;
-        public static double lon1;
+        public static double trackLat;
+        public static double trackLon;
         public static double desiredRadius;
         public static int maxFileData = 5000000;
         public static List<Information> fileData = new List<Information>(maxFileData);
-        public static double pi = 3.1415926;
+        public static double pi = 3.14159265378;
 
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
             string[] values = input.Split(',');
             desiredRadius = double.Parse(Console.ReadLine());
-            lat1 = double.Parse(values[0]);
-            lon1 = double.Parse(values[1]);
-            lat1 = lat1 * pi / 180;
-            lon1 = lon1 * pi / 180;
+            trackLat = ToRadiands(double.Parse(values[0]));
+            trackLon = ToRadiands(double.Parse(values[1]));
 
             ReadSubscribersData();
+
             var size = fileData.Count;
-            var printComma = false;
             fileData.Sort((a, b) => { return a.phoneNumber > b.phoneNumber ? 1 : a.phoneNumber < b.phoneNumber ? -1 : 0; });
-            if (size == 0)
-            {
-                throw new Exception("Fuck Off");
-            }
+
             for (int i = 0; i < size; i++)
             {
-                if (printComma)
+                if (i > 0)
                 {
                     Console.Write(",");
                 }
 
-                printComma = true;
                 Console.Write(fileData[i].phoneNumber);
             }
         }
 
+        public static double ToRadiands(double degree)
+        {
+            double mid = 180;
+            return degree * pi / mid;
+        }
+
         public static double CalcDistance(double lat, double lon)
         {
-            lat = lat * pi / 180;
-            lon = lon * pi / 180;
-            var sinLat = Math.Sin((lat - lat1) / 2);
-            var sinLon = Math.Sin((lon - lon1) / 2);
-            return 2 * r * Math.Asin(Math.Sqrt(Math.Pow(sinLat, 2) + Math.Cos(lat1) * Math.Cos(lat) * Math.Pow(sinLon, 2)));
+            lat = ToRadiands(lat);
+            lon = ToRadiands(lon);
+            double two = 2;
+            double sinLat = Math.Sin((lat - trackLat) / two);
+            double sinLon = Math.Sin((lon - trackLon) / two);
+            return two * r * Math.Asin(Math.Sqrt(Math.Pow(sinLat, two) + Math.Cos(trackLat) * Math.Cos(lat) * Math.Pow(sinLon, two)));
         }
 
         public static void ReadSubscribersData()
         {
-            Console.ReadLine();
+            Console.ReadLine();//for the describe raw
             string input;
             string[] values;
             while (!string.IsNullOrEmpty((input = Console.ReadLine())))
@@ -81,7 +82,7 @@ namespace FoodTruck
                 var time = DateTime.ParseExact(values[0], format, culture);
                 var phoneNumber = long.Parse(values[3]);
 
-                if (dist <= desiredRadius)
+                if (dist < desiredRadius)
                 {
                     var info = fileData.Find(item => item.phoneNumber == phoneNumber);
                     if (info == null)

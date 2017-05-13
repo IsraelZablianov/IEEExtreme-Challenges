d85e7418b42c3bfd0f294725a8bb6a84728719cf
Israel Zablianov <123hartk123@gmail.com>
2017-05-13 19:41:49 +0300

    Added Digit Fun

---
+++ DigitFun/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = string.Empty;
+            List<string> values = new List<string>(10000);
+            while (input != "END")
+            {
+                input = Console.ReadLine();
+                if(input != "END")
+                {
+                    values.Add(input);
+                }
+            }
+
+            foreach (var value in values)
+            {
+                Console.WriteLine(Result(value));
+            }
+        }
+
+        static int Result(string n)
+        {
+            var result = 1;
+            var sizeOfN = n.Length.ToString();
+            if (sizeOfN == n)
+            {
+                return result;
+            }
+
+            for (result = 2; sizeOfN != "1"; result++)
+            {
+                if (sizeOfN.Length.ToString() != sizeOfN)
+                {
+                    sizeOfN = sizeOfN.Length.ToString();
+                }
+            }
+            
+            return result;
+        }
+    }
+}
